7963c2ac4adabfbec417f87e018d3dd237fac8dd
cypher04 <seyi446@gmail.com>
2022-09-11 16:32:54 -0700

    C programs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-*main - Entry
+*main - prints the phrase "Prograamming is like Building a multilingual puzzle"
 *
-*return: 0
+*Return: Always 0 if exited properly, no-zero otherwise
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
