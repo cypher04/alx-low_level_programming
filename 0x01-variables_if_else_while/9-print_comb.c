0a4098ff271f3aa3a5cd7a174f1311ee7c24705c
cypher04 <seyi446@gmail.com>
2022-09-11 20:20:31 -0700

    new file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point into program
+ *
+ * Return: 0 Always (sucess)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 10; i < 20; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i != 19)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
